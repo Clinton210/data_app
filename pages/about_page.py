7a907a796bbaa87be7441b89bd4aefeae6cbcda2
Clinton Potter <clint_potter210@yahoo.com>
2020-12-01 13:49:38 -0600

    Changed link to github in about page

--- pages/about_page.py
+++ pages/about_page.py
@@ -5,7 +5,7 @@ def about_main():
     st.subheader("This open source app is written in Python with Streamlit.")
     st.subheader("")
     with st.beta_expander("Source Files"):
-        st.markdown("[GitHub Page](https://github.com/clinton210)")
+        st.markdown("[GitHub Page](https://github.com/Clinton210/data_app)")
     with st.beta_expander("Streamlit Info"):
         st.markdown("[Streamlit Website](https://www.streamlit.io/)")
     with st.beta_expander("Author: Clinton Potter"):
